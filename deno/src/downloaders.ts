d0e2dfd6c985544b126bcf033c75c17b67628947
Mohammed <88824957+majhcc@users.noreply.github.com>
2022-02-17 18:42:01 +0400

    *

--- deno/src/downloaders.ts
+++ deno/src/downloaders.ts
@@ -1,20 +1,20 @@
 export const twitter = async (url: string) => {
-    const response = await fetch(`https://server1.majhcc.xyz/api/tw?url=` + url);
+    const response = await fetch(`https://api-v1.majhcc.com/api/tw?url=` + url);
     const data = await response.json();
     return data;
 }
 export const tiktok = async (url: string) => {
-    const response = await fetch(`https://server1.majhcc.xyz/api/tk?url=` + url);
+    const response = await fetch(`https://api-v1.majhcc.com/api/tk?url=` + url);
     const data = await response.json();
     return data;
 }
 export const youtube = async (url: string) => {
-    const response = await fetch(`https://server1.majhcc.xyz/api/yt?url=` + url);
+    const response = await fetch(`https://api-v1.majhcc.com/api/yt?url=` + url);
     const data = await response.json();
     return data;
 }
 export const snapchat = async (username: string) => {
-    const response = await fetch(`https://server1.majhcc.xyz/api/snapchat?username=` + username);
+    const response = await fetch(`https://api-v1.majhcc.com/api/snapchat?username=` + username);
     const data = await response.json();
     return data;
 }
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 const twitter = async (url) => {
-    const response = await axios.get(`https://server1.majhcc.xyz/api/tw?url=` + url);
+    const response = await axios.get(`https://api-v1.majhcc.com/api/tw?url=` + url);
     const data = await response.data;
     return data;
 }
 const tiktok = async (url) => {
-    const response = await axios.get(`https://server1.majhcc.xyz/api/tk?url=` + url);
+    const response = await axios.get(`https://api-v1.majhcc.com/api/tk?url=` + url);
     const data = await response.data;
     return data;
 }
 const youtube = async (url) => {
-    const response = await axios.get(`https://server1.majhcc.xyz/api/yt?url=` + url);
+    const response = await axios.get(`https://api-v1.majhcc.com/api/yt?url=` + url);
     const data = await response.data;
     return data;
 }
 const snapchat = async (username) => {
-    const response = await axios.get(`https://server1.majhcc.xyz/api/snapchat?username=` + username);
+    const response = await axios.get(`https://api-v1.majhcc.com/api/snapchat?username=` + username);
     const data = await response.data;
     return data;
 }
